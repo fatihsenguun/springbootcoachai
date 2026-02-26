62d518533b21809bf7b134ba6dfc1694074e12d9
fatihsenguun <fatihsenguun@gmail.com>
2026-02-26 08:10:41 +0300

    initial commit

---
+++ src/main/java/com/fatihsengun/coachai/CoachaiApplication.java
@@ -0,0 +1,13 @@
+package com.fatihsengun.coachai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoachaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoachaiApplication.class, args);
+	}
+
+}
